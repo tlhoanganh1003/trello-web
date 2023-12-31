cfb2823b728dd0170a3fdceb33ba32f56eb37a49
tlhoanganh1003 <tlhoanganh1003@gmail.com>
2023-12-31 23:13:10 +0700

    appbar and boardbar

--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -1,14 +1,107 @@
 import Box from '@mui/system/Box'
+import Chip from '@mui/material/Chip'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import VpnLockIcon from '@mui/icons-material/VpnLock'
+import AddToDriveIcon from '@mui/icons-material/AddToDrive'
+import BoltIcon from '@mui/icons-material/Bolt'
+import FilterListIcon from '@mui/icons-material/FilterList'
+import Avatar from '@mui/material/Avatar'
+import AvatarGroup from '@mui/material/AvatarGroup'
+import Tooltip from '@mui/material/Tooltip'
+import Button from '@mui/material/Button'
+import PersonAddIcon from '@mui/icons-material/PersonAdd'
+
+const MENU_STYLES = {
+  color: 'primary.main',
+  bgcolor: 'white',
+  border: 'none',
+  paddingX: '5px',
+  borderRadius: '4px',
+  '& .MuiSvgIcon-root': {
+    color: 'primary.main'
+  },
+  '&:hover': {
+    bgcolor: 'primary.50'
+  }
+
+}
 
 function BoardBar() {
   return (
     <Box sx={{
-      backgroundColor: 'primary.dark',
+      backgroundColor: 'white',
       width:'100%',
       height: (theme) => theme.trello.boardBarHeight,
       display: 'flex',
-      alignItems: 'center'
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      gap: 2,
+      paddingX: 2,
+      overflowX: 'auto',
+      borderTop: '1px solid #00bfa5'
     }}>
+      <Box sx={{ display:'flex', alignItems:'center', gap: 2 }}>
+        <Chip
+          sx={MENU_STYLES}
+          icon={<DashboardIcon />} label="HoangAnh Board"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<VpnLockIcon />}
+          label="Public/Private Workspace"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<AddToDriveIcon />}
+          label="Add To Google Drive"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<BoltIcon />}
+          label="Automation"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<FilterListIcon />}
+          label="Filtes"
+          clickable
+        />
+      </Box>
+      <Box sx={{ display:'flex', alignItems:'center', gap: 2 }}>
+        <Button variant="outlined" startIcon={<PersonAddIcon />}>Invite</Button>
+        <AvatarGroup max={4}
+          sx={{
+            '& .MuiAvatar-root': {
+              width: 34,
+              height: 34,
+              fontSize: 16
+            }
+          }}
+        >
+          <Tooltip title="hoanganh">
+            <Avatar
+              alt="iue"
+              src='https://www.facebook.com/photo/?fbid=849371812755249&set=a.103272610698510'
+            />
+          </Tooltip>
+          <Tooltip title="hoanganh">
+            <Avatar
+              alt="iue"
+              src='https://www.facebook.com/photo/?fbid=849371812755249&set=a.103272610698510'
+            />
+          </Tooltip>
+          <Tooltip title="hoanganh">
+            <Avatar
+              alt="iue"
+              src='https://www.facebook.com/photo/?fbid=849371812755249&set=a.103272610698510'
+            />
+          </Tooltip>
+        </AvatarGroup>
+      </Box>
         Board Bar
     </Box>
   )
--- src/theme.js
+++ src/theme.js
@@ -21,8 +21,70 @@ const theme = extendTheme({
         secondary:orange
       }
     }
-  }
+  },
   // ...other properties
+  components: {
+    // Name of the component
+    MuiCssBaseline: {
+      styleOverrides:{
+        body: {
+          '*::-webkit-scrollbar': {
+            width: '8px',
+            height: '8px'
+          },
+          '*::-webkit-scrollbar-thumb': {
+            backgroundColor: '#bdc3c7',
+            borderRadius: '8px'
+          },
+          '*::-webkit-scrollbar-thumb:hover': {
+            backgroundColor: '#00b894'
+          }
+        }
+      }
+    },
+    MuiButton: {
+      styleOverrides: {
+        // Name of the slot
+        root: {
+          // Some CSS
+          textTransform: 'none'
+        }
+      }
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        // Name of the slot
+        root: ({ theme }) => ({
+          // Some CSS
+          color: theme.palette.primary.main,
+          fontSize: '0.875rem'
+        })
+      }
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        // Name of the slot
+        root: ({ theme }) => {
+          // Some CSS
+          return {
+            color: theme.palette.primary.main,
+            fontSize: '0.875rem',
+            '.MuiOutlinedInput-notchedOutline':{
+              boderColor: theme.palette.primary.light
+            },
+            '&:hover': {
+              '.MuiOutlinedInput-notchedOutline':{
+                boderColor: theme.palette.primary.main
+              }
+            },
+            '& feildset': {
+              boderWidth: '1px !important'
+            }
+          }
+        }
+      }
+    }
+  }
 })
 
 export default theme
